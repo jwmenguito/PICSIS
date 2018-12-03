a7ac2f0d91d16cc4f4a5445b06ce755542d1cb70
jwmenguito <jwmenguito@up.edu.ph>
2018-12-04 02:05:15 +0800

    Heh

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     'secret': 'picsisdemo',
-    'database': 'mongodb://jmenguito:Youngblood18@ds159670.mlab.com:59670/heroku_9t8tj7k2',
+    'database': 'mongodb://james:123qwe18@ds159670.mlab.com:59670/heroku_9t8tj7k2',
 	'year': 2018,
 	'term':1
 };
