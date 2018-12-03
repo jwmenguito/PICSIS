aaa73c7e77efa404f5c864ea13302503f81b1c31
jwmenguito <jwmenguito@up.edu.ph>
2018-12-04 01:17:56 +0800

    Added DB

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     'secret': 'picsisdemo',
-    'database': 'mongodb://localhost/PICSIS',
+    'database': 'mongodb://jmenguito:Youngblood18@ds159670.mlab.com:59670/heroku_9t8tj7k2',
 	'year': 2018,
 	'term':1
 };
