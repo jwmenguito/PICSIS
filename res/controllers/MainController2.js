5757518a105f25d9a6d4370e5f4a8e7fe3ebad08
James Paolo Menguito <jmenguito@exist.com>
2018-12-05 03:57:51 +0800

    Sana <3

--- res/controllers/MainController2.js
+++ res/controllers/MainController2.js
@@ -627,7 +627,7 @@ var homeModule = angular.module('homeModule',['ui.router','ui.bootstrap','ngCook
             $scope.load = function(){
                 var data = {
                     password: $scope.password,
-                    email: dataHolder().get(j).email;
+                    email: dataHolder().get(j).email
 	            }
 	            
 	            alert(data);
