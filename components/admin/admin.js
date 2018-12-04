ffee02f200037081c076d492251d98e01bfa65d8
James Paolo Menguito <jmenguito@exist.com>
2018-12-05 01:50:24 +0800

    Heh

--- components/admin/admin.js
+++ components/admin/admin.js
@@ -452,8 +452,13 @@ exports.admin_listing_remove = (req,res) => {
      var course = req.body.course;
      var major_degree = req.body.major_degree;  
      var subject_code = req.body.subject_code;
-     
-      Listing.update({"term":curr_term,"degree":course,"major_degree":major_degree,"subject_code":subject_code},{$pull:{students:std}},{multi:true},function(err,doc){
+      
+      Listing.find({"term":curr_term,"degree":course,"major_degree":major_degree,"subject_code":subject_code}).exec(function(err,docs){
+        if(err) throw err;
+        if(!docs) console.log("Empty");
+        if(docs) console.log(docs);
+      });
+      Listing.update({"term":curr_term,"degree":course,"major_degree":major_degree,"subject_code":subject_code},{$pullAll:{students:std}},{multi:true},function(err,doc){
             //console.log(doc);
 		    if(err) throw err;
 		    if(doc) res.json({message:"REMOVED from listing student number " + std +" from "+doc.nModified+" subject(s)"});
@@ -470,8 +475,15 @@ exports.admin_listing_remove_all = (req,res) => {
      var std = req.body.student_no;
      var course = req.body.course;
      var major_degree = req.body.major_degree;  
+       
+       Listing.find({"term":curr_term,"degree":course,"major_degree":major_degree,"subject_code":subject_code}).exec(function(err,docs){
+        if(err) throw err;
+        if(!docs) console.log("Empty");
+        if(docs) console.log(docs);
+      }); 
+       
      
-      Listing.update({"term":curr_term,"degree":course,"major_degree":major_degree},{$pull:{students:std}},{multi:true},function(err,doc){
+      Listing.update({"term":curr_term,"degree":course,"major_degree":major_degree},{$pullAll:{students:std}},{multi:true},function(err,doc){
             //console.log(doc);
 		    if(err) throw err;
 		    if(doc) res.json({message:"REMOVED from listing student number " + std +" from "+doc.nModified+" subjects"});
